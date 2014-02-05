8da45e79a7b4f57edece4038f496b99d0708dc0a
Patrick Leahy <patrick@patrickleahy.org>
2014-02-05 13:59:40 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,170 @@
+
+/**
+ * Module dependencies.
+
+ //running node on production
+ nohup node app.js &
+ 54.221.206.92
+ */
+var mysql = require('mysql');
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+var app = express();
+
+// Other dependencies
+var passwordHash = require('password-hash');
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.cookieParser('your secret here'));
+app.use(express.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(app.get('env'));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// var db = exports.db = mysql.createConnection({host:'localhost', user:'root', database:'orderAhead', supportBigNumbers:true, typeCast:true});
+var db = exports.db = mysql.createConnection({host:'orderahead.crvlki5fknye.us-east-1.rds.amazonaws.com',port: 3306, user:'orderAhead',password:'orderAhead1414',database:'orderAhead',supportBigNumbers:true, typeCast:true});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+app.post('/login', function(req, res) {
+  console.log(JSON.stringify(req.body));
+  db.query('SELECT * FROM users WHERE email = ?', req.body.email, function (err, rows, result) {
+    if (err) {
+      res.send(JSON.stringify({'statusCode': 500, 'message': err}));
+    } else {
+      if (rows.length > 0) {
+        if (passwordHash.verify(req.body.password, rows[0].hashedPassword) === true) {
+          getInitialData(rows[0], function(body) {
+            res.send(JSON.stringify({'statusCode': 200, 'body': body}));  
+          });
+        } else {
+          res.send(JSON.stringify({'statusCode': 401, 'message': 'Password incorrect'}));  
+        }
+      } else {
+        res.send(JSON.stringify({'statusCode': 404, 'message': 'No user with that email address'}));
+      }
+    }
+  });
+});
+
+app.post('/cookieLogin', function(req, res) {
+  console.log(JSON.stringify(req.body));
+  db.query('SELECT * FROM users WHERE email = ?', req.body.email, function (err, rows, result) {
+
+    getInitialData(rows[0], function(body) {
+      res.send(JSON.stringify({'statusCode': 200, 'body': body}));  
+    });
+  });
+});
+
+app.post('/signUp', function(req, res) {
+  req.body.hashedPassword = passwordHash.generate(req.body.password);
+  delete req.body.password;
+
+  db.query('INSERT INTO users SET ?', req.body, function (err, result) {
+    if (err) {
+      res.send(JSON.stringify({'statusCode': 500}));
+    } else {
+      req.body.id = result.insertId;
+      getInitialData(req.body, function(body) {
+        res.send(JSON.stringify({'statusCode': 200, 'body': body}));  
+      });
+    }
+  });
+});
+
+app.post('/statusUpdate', function(req, res) {
+  console.log('INSERT INTO news SET ' + JSON.stringify(req.body));
+  db.query('INSERT INTO news SET ?', req.body, function (err) {
+    if (err) {
+      res.send(JSON.stringify({'statusCode': 500}));
+    } else {
+      res.send(JSON.stringify({'statusCode': 200, 'body': req.body}));
+    }
+  });
+});
+
+app.post('/friendsPage', function(req, res) {
+  console.log(req.body);
+  db.query('SELECT * FROM news WHERE user_id = ?', req.body.user_id, function (err, news, results) {
+
+    db.query('SELECT * FROM users WHERE id = ?', req.body.user_id, function (err, user, results) {
+
+      delete user[0]['hashedPassword'];
+      var body = { 'user': user[0], 'news': news };
+      if (err) {
+        res.send(JSON.stringify({'statusCode': 500}));
+      } else {
+        res.send(JSON.stringify({'statusCode': 200, 'body': body}));
+      }
+    });
+
+  });
+});
+
+app.post('/viewNews', function(req, res) {
+  db.query('SELECT * FROM news', function (err, news, results) {
+    var body = { 'news': news };
+    console.log(JSON.stringify(body));
+    if (err) {
+      res.send(JSON.stringify({'statusCode': 500}));
+    } else {
+      res.send(JSON.stringify({'statusCode': 200, 'body': body}));
+    }
+  });
+});
+
+app.post('/createFriendship', function(req, res) {
+  db.query('SELECT * FROM friendships WHERE friend_one = ? && friend_two = ?', [req.body.friend_one, req.body.friend_two], function (err, firstCombo, results) {
+    db.query('SELECT * FROM friendships WHERE friend_two = ? && friend_one = ?', [req.body.friend_one, req.body.friend_two], function (err, secondCombo, results) {
+      if (firstCombo.length > 0 || secondCombo.length > 0) {
+        res.send(JSON.stringify({'statusCode': 404, 'message': 'Friendship already pending'}));
+      } else {
+        db.query('INSERT INTO friendships SET ?', req.body, function (err, results) {
+          if (err) {
+            res.send(JSON.stringify({'statusCode': 500, 'message': 'Internal database error: ' + err}));
+          } else {
+            res.send(JSON.stringify({'statusCode': 200}));
+          }
+        });
+      }
+    });
+  });
+});
+
+function getInitialData(myData, callback) {
+  if (myData !== null) {
+    delete myData.hashedPassword;
+    db.query('SELECT id, firstName, lastName, email FROM users', function (err, users, result) {
+      db.query('SELECT * FROM news', function (err, news, result) {
+        if (err) throw err;
+        var body = { 'myData': myData, 'users': users, 'news': news };
+        callback(body);
+      });
+    });    
+  }
+}
+
+
+
---
+++ public/javascripts/main.js
@@ -0,0 +1,233 @@
+$(document).ready(function() {
+
+  $('#leftColumn').height($(document).height());
+
+  // Automatically log the user in if the login cookie is present
+  if ($.cookie('myData') !== undefined) {
+    var data = $.param(JSON.parse($.cookie('myData')));
+
+    makeAjaxRequest('/cookieLogin', data, function (res) {
+      loginRoutine(res);
+    });
+  }
+
+  $("form#login").on("submit", function(event) {
+    event.preventDefault();
+
+    if (nullFieldsCheck($(this))) {
+      var data = $(this).serialize();
+      makeAjaxRequest('/login', data, function (res) {
+        loginRoutine(res);
+      });
+    } else {
+      displayError("Warning! No fields may be left blank");
+    }
+
+  });
+
+  $("form#signUp").on("submit", function(event) {
+    event.preventDefault();
+
+    if (nullFieldsCheck($(this))) {
+      var data = $(this).serialize();
+      makeAjaxRequest('/signUp', data, function (res) {
+        loginRoutine(res);
+      });
+    } else {
+      displayError("Warning! No fields may be left blank");
+    }
+  });
+
+  $("form#statusUpdate").on("submit", function(event) {
+    event.preventDefault();
+
+    if (nullFieldsCheck($(this))) {
+      var data = $(this).serialize();
+      makeAjaxRequest('/statusUpdate', data, function (res) {
+        var res = JSON.parse(res);
+        $('#wall').append('<div class="wallItem"><p>' + res.body.body + '</p></div>');
+      });
+    } else {
+      displayError("Warning! Status update cannot be null");
+    }
+  });
+
+  $('#logOut').on("click", function(event) {
+    event.preventDefault();
+    $.removeCookie('authToken');
+    $('#innerPages').hide();
+    $('#splashPage').show();
+  });
+
+  $('#viewDirectory').on("click", function(event) {
+    event.preventDefault();
+
+    $("#newsItems").hide();
+    $("#directoryItems").show();
+    $("#profilePage").hide();
+    $('#friendProfile').hide();
+
+  });
+
+  $('.viewMyProfile').on("click", function(event) {
+    event.preventDefault();
+
+    var data = {'user_id': $(this).attr('href').split('/')[2]};
+    makeAjaxRequest('/friendsPage', data, function (res) {
+      var res = JSON.parse(res);
+      $('#wall').empty();
+      for (var i = 0; i < res.body.news.length; i++) {
+        $('#wall').append('<div class="wallItem"><p>' + res.body.news[i].body + '</p></div>');
+      }
+
+      $("#newsItems").hide();
+      $("#directoryItems").hide();
+      $("#profilePage").show();
+      $('#friendProfile').hide();
+    });
+  });
+
+  $('.viewNewsFeed').on("click", function(event) {
+    event.preventDefault();
+
+    makeAjaxRequest('/viewNews', null, function (res) {
+      // console.log(res);
+
+      var res = JSON.parse(res);
+      fillNewsFeed(res.body.news);
+      $("#newsItems").show();
+      $("#directoryItems").hide();
+      $("#profilePage").hide();
+      $('#friendProfile').hide();
+    });
+
+  });
+
+  $('.friendMe').on("click", function(event) {
+    event.preventDefault();
+    var data = {'friend_one': JSON.parse($.cookie('myData')).id, 'friend_two': $(this).attr('href').split('/')[2]};
+
+    makeAjaxRequest('/createFriendship', data, function (res) {
+      var res = JSON.parse(res);
+      if (res.statusCode === 200) {
+        displayNotification("Friendship pending!");
+      } else if (res.statusCode === 404) {
+        displayError(res.message);
+      } else if (res.statusCode === 500) {
+        displayError(res.message);
+      }
+    });
+  });
+
+  $(document).on('click', '.viewFriendProfile', function(event) {
+    event.preventDefault();
+
+    var data = {'user_id': $(this).attr('href').split('/')[2]};
+    makeAjaxRequest('/friendsPage', data, function (res) {
+      console.log(res);
+      var res = JSON.parse(res);
+      var fullName = res.body.user.firstName + " " + res.body.user.lastName;
+      $('#friendName').text(fullName);
+      $('.friendMe').attr('href', '/users/' + res.body.user.id);
+
+      if (res.body.news > 0) {
+        for (var i = 0; i < res.body.news; i++) {
+          $('.friendWall').append('<div class="wallItem"><p>' + res.body.news[i].body + '</p></div>');
+        }        
+      } else {
+        $('.friendWall').append('<div class="wallItem"><p>No news</p></div>');
+      }
+
+      $("#newsItems").hide();
+      $("#directoryItems").hide();
+      $("#profilePage").hide();
+      $('#friendProfile').show();
+
+    });
+
+  });
+});
+
+function displayError (text) {
+  console.error(text);
+  $("#errors").show();
+  $('#errors p.alert').text(text);
+  $("#errors").fadeOut(2500);
+}
+
+function displayNotification (text) {
+  console.log(text);
+  $("#notifications").show();
+  $('#notifications p.alert').text(text);
+  $("#notifications").fadeOut(2500);
+}
+
+function nullFieldsCheck(formObject) {
+  var serializedForm = $(formObject).serializeArray();
+  var validity = true;
+  for (var i = 0; i < serializedForm.length; i++) {
+    if (serializedForm[i]['value'] === "") {
+      validity = false;
+    }
+  }
+  return validity;
+}
+
+function fillNewsFeed(news) {
+  $('#newsWrapper').empty();
+
+  for (var i = 0; i < news.length; i++) {
+    $('#newsWrapper').append('<div class="newsItem"><p>' + news[i].body + '</p></div>');
+  }
+}
+
+function fillDirectory(users) {
+  $('#directoryWrapper').empty();
+
+  for (var i = 0; i < users.length; i++) {
+    $('#directoryWrapper').append('<div class="directoryItem"><p><a href="/users/' + users[i].id + '" class="viewFriendProfile">' + users[i].firstName + " " + users[i].lastName + '</a></p></div>');
+  }
+}
+
+function fillLeftBar(myData) {
+  $('input.user_id').val(myData.id);
+  $('.heavy.viewMyProfile').text(myData.firstName + " " + myData.lastName);
+  $('.heavy.viewMyProfile').attr('href', '/users/' + myData.id);
+  $('.viewMyProfile.link').attr('href', '/users/' + myData.id);
+}
+
+function makeAjaxRequest (url, data, callback) {
+  request = $.ajax({
+    url: url,
+    type: "post",
+    data: data
+  });
+
+  request.done(function (response, textStatus, jqXHR) {
+    callback(response);
+  });
+
+  request.fail(function (jqXHR, textStatus, errorThrown) {
+    displayError("The following error occured: " + textStatus + " " + errorThrown);
+  });
+}
+
+function loginRoutine (res) {
+  console.log(res);
+  var res = JSON.parse(res);
+  if (res.statusCode === 200) {
+    $.cookie('myData', JSON.stringify(res.body.myData));
+    fillLeftBar(res.body.myData);
+    fillNewsFeed(res.body.news);
+    fillDirectory(res.body.users);
+
+    $('#splashPage').hide();
+    $('#innerPages').show();
+    $("#newsItems").show();
+    $("#directoryItems").hide();
+    $("#profilePage").hide();
+    $('#friendProfile').hide();
+  } else {
+    displayError(res.message);    
+  }
+}
